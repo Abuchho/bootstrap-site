52bb7b4bd580b188c589dcc5b2854ab50e97624f
Andrew Buchholz <learningemail33@gmail.com>
2021-09-10 03:09:27 -0400

    update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require ('express')
 const app = express()
+const request = require ('request')
 
 app.use(express.static('public'))
 app.set('view engine','ejs');
@@ -11,19 +12,15 @@ app.get('/', (req, res)=> {
     res.render('index')
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('/data', function(req, res, next) {
+    request({
+      uri: 'https://api.bls.gov/publicAPI/v1/timeseries/data/LAUMT131226000000003',
+      qs: {
+        api_key: '',
+        query: 'Population'
+      }
+    }).pipe(res);
+  });
 
 
 
---
+++ public/other.js
@@ -0,0 +1,14 @@
+// let xhr = new XMLHttpRequest();
+// xhr.onload = function(){
+//     let data = JSON.parse(this.responseText);
+//     console.log(data);
+    
+
+//     document.getElementById('content').innerHTML = 
+//     `<span>Stock Symbol: ${price.price.symbol} <br>
+//     Price: ${price.price.regularMarketPrice}</span>`
+
+
+// }
+// xhr.open('GET', 'https://datausa.io/api/covid19/employment/latest/');
+// xhr.send();
