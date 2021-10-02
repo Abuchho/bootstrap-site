ccb8b3395010edea1dd024d6b0cdbbe9ff7d9d5d
Andrew Buchholz <learningemail33@gmail.com>
2021-10-02 14:32:07 -0400

    removed ejs and fixed file path

--- public/events.js
+++ public/events.js
@@ -1,56 +1,3 @@
-
-const express = require ('express')
-const app = express()
-const axios = require ('axios')
-
-app.use(express.static('public'))
-app.set('view engine','ejs');
-
-
-let url = 'http://api.census.gov/data/2019/pep/population?get=NAME,POP&for=state:*';
-
-
-app.get('/', (req, res)=> {
-
-    (async () => {
-        try {
-            const response = await axios.get(url);
-            
-            console.log(response);
-            
-           
-            res.render('index',{
-                statePop : response.data
-            });
-        } catch (error) {
-            console.error(error);
-            
-        }
-    })();
-
-
-});
-
-
-
-app.listen(3000);
-
-
-// Population of every State 'http://api.census.gov/data/2019/pep/population?get=NAME,POP&for=state:*' 
-// Population for counties 'https://api.census.gov/data/2019/pep/population?get=NAME,POP&for=county:*&in=state:*'
-
-         //replace * with state FIPS code to get counties for any state
-
-// Poverty rate per state 'https://api.census.gov/data/timeseries/poverty/saipe?get=SAEPOVRTALL_PT,NAME&for=state:*&time=2019'
-
-// Poverty rate for all states and all counties 'https://api.census.gov/data/timeseries/poverty/saipe?get=SAEPOVALL_PT,GEOID,NAME&for=county:*&in=state:*&time=2019'
-
-//Unemployment rates 'https://api.bls.gov/publicAPI/v2/timeseries/data/LAUST130000000000003' 
-
-// LAU${'put state code here'}03
-
-
-// State FIPS code to match with state names 
 let stateFips = {
     "Alabama"                :  "01",
     "Alaska"                 :  "02",
@@ -104,5 +51,3 @@ let stateFips = {
     "Wisconsin"              :  "55",
     "Wyoming"                :  "56"
  };
- 
- 
